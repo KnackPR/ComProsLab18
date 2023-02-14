2aadba2011aecfc3a66d51aa596866c792fb5892
knackpr <peerawich_r@cmu.ac.th>
2023-02-14 16:18:16 +0700

    สบายๆ

--- lab18_3.cpp
+++ lab18_3.cpp
@@ -4,9 +4,26 @@
 #include<vector>
 using namespace std;
 
-vector<int> randomVector(int);
-void showVector(vector<int>);
-int dotProduct(vector<int>,vector<int>);
+vector<int> randomVector(unsigned int N){	
+    vector<int> v(N);
+    for(unsigned int i = 0; i < N; i++) v.at(i) = (rand()%10);
+    return v;
+}
+
+void showVector(vector<int> v){
+	cout << "[";
+	for(unsigned int i = 0; i < v.size(); i++){
+		cout << v.at(i);
+		if(i == v.size() - 1) cout << "]";
+		else cout << " ";
+	}
+}
+
+int dotProduct(vector<int> a, vector<int> b){
+	int sum = 0;
+	for(unsigned int i = 0; i < a.size(); i++) sum += a.at(i)*b.at(i);	
+	return sum;
+}
 
 int main(){
 	srand(time(0));
@@ -20,5 +37,3 @@ int main(){
 	
 	return 0;
 }
-
-//Write Definition of randomVector() showVector() and dotProduct() here
