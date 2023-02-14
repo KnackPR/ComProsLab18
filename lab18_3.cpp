0457fb30feee965bbf10eb78c27637d7d7ac4673
knackpr <peerawich_r@cmu.ac.th>
2023-02-14 16:21:30 +0700

    สบายๆ

--- lab18_3.cpp
+++ lab18_3.cpp
@@ -4,7 +4,24 @@
 #include<vector>
 using namespace std;
 
-vector<int> randomVector(unsigned int N){	
+vector<int> randomVector(int);
+void showVector(vector<int>);
+int dotProduct(vector<int>,vector<int>);
+
+int main(){
+	srand(time(0));
+	vector<int> x = randomVector(5);
+	vector<int> y = randomVector(5);
+	int z = dotProduct(x,y);
+	showVector(x);
+	cout << " . ";
+	showVector(y);
+	cout << " = " << z;
+	
+	return 0;
+}
+
+vector<int> randomVector(int N){	
     vector<int> v(N);
     for(unsigned int i = 0; i < N; i++) v.at(i) = (rand()%10);
     return v;
@@ -23,17 +40,4 @@ int dotProduct(vector<int> a, vector<int> b){
 	int sum = 0;
 	for(unsigned int i = 0; i < a.size(); i++) sum += a.at(i)*b.at(i);	
 	return sum;
-}
-
-int main(){
-	srand(time(0));
-	vector<int> x = randomVector(5);
-	vector<int> y = randomVector(5);
-	int z = dotProduct(x,y);
-	showVector(x);
-	cout << " . ";
-	showVector(y);
-	cout << " = " << z;
-	
-	return 0;
 }
